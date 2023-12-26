2393ddb0797d510e9a800b6de9dd4c4d5398e5ba
Weed105 <123948948+Weed105@users.noreply.github.com>
2023-12-27 01:01:08 +0500

    1

--- TestApi1/Controllers/MemberController.cs
+++ TestApi1/Controllers/MemberController.cs
@@ -29,6 +29,29 @@ namespace TestApi1.Controllers
             ApplicationContext.SaveChanges();
             return Ok();
         }
+        [HttpDelete]
+        public ActionResult DeleteMembers([FromBody]Member member)
+        {
+            ApplicationContext.member.Remove(member);
+            ApplicationContext.SaveChanges();
+            return Ok();
+        }
+        [HttpPut]
+        public ActionResult PutMembers([FromBody]Member member)
+        {
+            Member member1 = ApplicationContext.member.FirstOrDefault(u => u.Id == member.Id);
+            if (member1 == null) return BadRequest(new {message = "Пользователь не найден"});
+            member1.Surname = member.Surname;
+            member1.Name = member.Name;
+            member1.Patronymic = member.Patronymic;
+            member1.Date = member.Date;
+            member1.Experiece = member.Experiece;
+            member1.Login = member.Login;
+            member1.Password = member.Password;
+            ApplicationContext.SaveChanges();
+            return Ok();
+
+        }
     }
 
 }
